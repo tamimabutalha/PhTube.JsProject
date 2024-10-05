3b4a4dad5e8ed900f204dc103c2235b458a9bf98
tamim <abutalhatamim017@gmail.com>
2024-10-05 23:11:12 +0600

    this project was not completed

--- scripts/video.js
+++ scripts/video.js
@@ -1,4 +1,27 @@
-const loadCategories = ()  => {
+const loadCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories))
+        .catch((error) => console.log(error));
+};
+const loadVideos = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res) => res.json())
+        .then((data) => console.log (data))
+        .catch((error) => console.log(error));
+};
 
-    fetch('')
-}
+const displayCategories = (categories) => {
+    const categoriesContainer = document.getElementById('categories');
+    categories.forEach((item) => {
+        console.log(item);
+        const button = document.createElement("button");
+        button.classList = "btn";
+        button.innerText = item.category; // Corrected from innertext to innerText
+
+        categoriesContainer.append(button);
+    });
+};
+
+loadCategories();
+loadVideos();
